7d3aa4c98db19d2822443cdb99daa66da1a7336e
Guilherme Salustiano <guissalustiano@gmail.com>
2020-12-13 03:40:12 -0300

    Deixando uma polybar descente

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,9 +5,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-## Wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-## Launch
-polybar mybar &
-
+# Launch bar1 and bar2
+polybar -c ~/.config/polybar/config.ini main &
