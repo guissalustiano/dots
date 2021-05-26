340c977dc21f8f46bcd1de96ea5551e963816ba5
Guilherme Salustiano <guissalustiano@gmail.com>
2021-05-26 12:13:20 -0300

    organize zsh

---
+++ zsh/vars.zsh
@@ -0,0 +1,12 @@
+export EDITOR='nvim'
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+# Path
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/.yarn/bin:$PATH
+export PATH=$HOME/.gem/ruby/2.7.0/bin:$PATH
+export PATH=$HOME/.pub-cache/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.stack/global-project/stack.yaml:$PATH
+export PATH=$HOME/.dotnet/tools:$PATH
