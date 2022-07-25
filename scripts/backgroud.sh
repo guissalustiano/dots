86e82acbde738e629962c9d2a6ccb51a48dea134
Guilherme Salustiano <guissalustiano@gmail.com>
2022-07-25 00:03:54 -0300

    add save backgroud script

--- scripts/backgroud.sh
+++ scripts/backgroud.sh
@@ -1,8 +1,7 @@
-!# /bin/bash
+#! /bin/bash
 
 IMAGE_PATH='/tmp/bg.jpeg'
 IMAGE_URL='https://source.unsplash.com/random/2560x1080'
 
 curl -L -o $IMAGE_PATH $IMAGE_URL
 feh --bg-fill $IMAGE_PATH 
-rm $IMAGE_PATH
---
+++ scripts/save_backgroud.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+IMAGE_PATH='/tmp/bg.jpeg'
+PERSIST_IMAGE_PATH="$HOME/Images/system/bg.jpeg"
+
+mv $IMAGE_PATH $PERSIST_IMAGE_PATH
