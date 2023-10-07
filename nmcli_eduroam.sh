3e675cec4784ad01ff76608d53ff4926fde3d60b
Guilherme S. Salustiano <guissalustiano@gmail.com>
2023-10-07 03:23:32 +0200

    add nmcli eduaroam config

---
+++ nmcli_eduroam.sh
@@ -0,0 +1,13 @@
+nmcli con add \
+  type wifi \
+  con-name "eduroam" \
+  ifname "wlp0s20f3" \
+  ssid "eduroam" \
+  wifi-sec.key-mgmt "wpa-eap" \
+  802-1x.identity "g.stabachsalustiano@student.utwente.nl" \
+  802-1x.password "<password>" \
+  802-1x.system-ca-certs "yes" \
+  802-1x.eap "peap" \
+  802-1x.phase2-auth "mschapv2"
+
+  nmcli connection up eduroam --ask
