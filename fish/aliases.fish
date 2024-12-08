d183a03293f158d3aa1f3ee7076a030b07313f48
Guilherme Stabach Salustiano <guissalustiano@gmail.com>
2024-12-08 18:56:14 -0300

    save

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -4,15 +4,14 @@ alias la="eza -la"
 alias tree="eza -tree"
 
 # Editor
-alias vi="nvim"
-alias hx="helix"
+#alias hx="helix"
 
 abbr -a vitw -- vi ~/dots/hyprland.conf
 
 
 # Git
 abbr -a -- g git
-abbr -a -- ga 'git add'
+abbr -a -- ga 'git add -p'
 abbr -a -- gc 'git commit -v'
 abbr -a -- gc! 'git commit --verbose --amend'
 abbr -a -- gcn! 'git commit --verbose --no-edit --amend'
@@ -25,8 +24,8 @@ abbr -a -- gp 'git push'
 abbr -a -- gpf 'git push --force-with-lease'
 abbr -a -- gpsup 'git push --set-upstream origin $(git branch 2> /dev/null | sed -e \'/^[^*]/d\' -e \'s/* \\(.*\\)/\\1/\')'
 abbr -a -- gl 'git pull'
-abbr -a -- grs 'git restore'
-abbr -a -- grst 'git restore --staged'
+abbr -a -- grs 'git restore -p'
+abbr -a -- grst 'git restore --staged -p'
 abbr -a -- gst 'git status'
 abbr -a -- gsta 'git stash'
 abbr -a -- grb 'git rebase'
--- fish/config.fish
+++ fish/config.fish
@@ -6,18 +6,31 @@ source ~/dots/fish/aliases.fish
 
 
 fish_add_path ~/.cargo/bin # Rust
-fish_add_path ~/.local/bin # Python
 fish_add_path ~/go/bin # go
 fish_add_path ~/bin # scripts
 fish_add_path /opt/google-cloud-cli/bin # gcloud
+fish_add_path ~/dev/flutter/bin
+fish_add_path ~/.local/bin # Python
+fish_add_path ~/bin/google-cloud-sdk/bin
 
 # Android
-set -gx ANDROID_HOME /opt/android-sdk
-fish_add_path $ANDROID_HOME/platform-tools/
-fish_add_path $ANDROID_HOME/tools/bin/
-fish_add_path $ANDROID_HOME/tools/
+fish_add_path /home/guiss/Android/Sdk/platform-tools/
+fish_add_path /home/guiss/Android/Sdk/
+fish_add_path /home/guiss/Android/Sdk/cmdline-tools/latest/bin/
 
 starship init fish | source # starship
 zoxide init fish | source # zoxide
 #atuin init fish | source # atuin
 kubectl completion fish | source # kubectl
+source $HOME/.grit/bin/env.fish
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/guiss/bin/google-cloud-sdk/path.fisn.inc' ]
+    . '/home/guiss/bin/google-cloud-sdk/path.fisn.inc'
+end
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/guiss/bin/google-cloud-sdk/completion.fisn.inc' ]
+    . '/home/guiss/bin/google-cloud-sdk/completion.fisn.inc'
+end
